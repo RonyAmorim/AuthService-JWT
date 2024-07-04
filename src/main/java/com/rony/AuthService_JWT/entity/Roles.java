6e810323a9791d2c3b066848af36aceadbce12b6
Rony Amorim dos Reis <ronyamorimdosreis@gmail.com>
2024-07-04 18:50:00 -0300

    feat: create User and Roles entities with fixed enum values
    - Implemented the `User` and `Roles` entities in the system.
    - Added an enum in the `Roles` entity for fixed values (ADMIN and BASIC).
    - Configured relationships and mappings between the entities.

---
+++ src/main/java/com/rony/AuthService_JWT/entity/Roles.java
@@ -0,0 +1,45 @@
+package com.rony.AuthService_JWT.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Setter
+@Getter
+@NoArgsConstructor
+public class Roles {
+
+    @Id
+    private long id;
+
+    private String description;
+
+
+    public Roles(long id, String name) {
+        this.id = id;
+        this.description = name;
+    }
+
+    public enum Values {
+        ADMIN(1, "ADMIN"),
+        USER(2, "USER");
+
+        private final long id;
+        private final String description;
+
+        Values(long id, String description) {
+            this.id = id;
+            this.description = description;
+        }
+
+        public Roles toRole() {
+            return new Roles(id, description);
+        }
+
+    }
+}
---
+++ src/main/java/com/rony/AuthService_JWT/entity/User.java
@@ -0,0 +1,85 @@
+package com.rony.AuthService_JWT.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private String phone;
+
+    @CreationTimestamp
+    private Instant created_at;
+
+    @CreationTimestamp
+    private Instant last_login;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Roles> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getDescription().toUpperCase()))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
