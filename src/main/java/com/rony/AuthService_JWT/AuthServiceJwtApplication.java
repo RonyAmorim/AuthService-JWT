e3492338dfe5587f0d10228e7cf0256fa298eaa9
Rony Amorim dos Reis <ronyamorimdosreis@gmail.com>
2024-07-04 18:47:39 -0300

    Initial commit

---
+++ src/main/java/com/rony/AuthService_JWT/AuthServiceJwtApplication.java
@@ -0,0 +1,13 @@
+package com.rony.AuthService_JWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceJwtApplication.class, args);
+	}
+
+}
